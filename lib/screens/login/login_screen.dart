763a5b967a75bd459d1986cbe6c3a0b3b46dd461
Ahmad Khan <rmahmadkhan@icloud.com>
2022-01-15 17:08:48 +0500

    Create LoginScreen

---
+++ lib/screens/login/login_screen.dart
@@ -0,0 +1,25 @@
+import 'package:chat_app/screens/login/components/my_text_field.dart';
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final _emailController = TextEditingController();
+
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              MyTextField(hint: 'Email', controller: _emailController),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
