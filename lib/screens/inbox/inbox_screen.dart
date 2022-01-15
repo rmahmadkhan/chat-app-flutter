e87ca92d39f22cf6e33b87784ae5471ffa0d7dc2
Ahmad Khan <rmahmadkhan@icloud.com>
2022-01-15 18:19:49 +0500

    Create InboxScreen

---
+++ lib/screens/inbox/inbox_screen.dart
@@ -0,0 +1,19 @@
+import 'package:chat_app/screens/inbox/components/chat_tile.dart';
+import 'package:flutter/material.dart';
+
+class InboxScreen extends StatelessWidget {
+  const InboxScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Inbox')),
+      body: ListView.separated(
+        itemCount: 3,
+        itemBuilder: (context, index) => const ChatTile('Name'),
+        separatorBuilder: (context, index) => const SizedBox(height: 10),
+        padding: const EdgeInsets.all(8.0),
+      ),
+    );
+  }
+}
