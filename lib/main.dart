4d09ece1885062453cb5059c70ce6cc99bcc4df2
Ahmad Khan <rmahmadkhan@icloud.com>
2022-01-16 16:12:03 +0500

    Added WidgetsFlutterBinding.ensureInitialized() to main

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
 
   runApp(const MyApp());
