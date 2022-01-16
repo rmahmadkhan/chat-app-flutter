0d5c760151cd2f9ddcc274cba6c4852fca96944a
Ahmad Khan <rmahmadkhan@icloud.com>
2022-01-16 15:51:57 +0500

    Created SignupScreen

---
+++ lib/screens/signup/signup_screen.dart
@@ -0,0 +1,43 @@
+import 'package:chat_app/screens/login/components/login_button.dart';
+import 'package:chat_app/widgets/my_text_field.dart';
+import 'package:flutter/material.dart';
+
+class SignupScreen extends StatelessWidget {
+  const SignupScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final _nameController = TextEditingController();
+    final _emailController = TextEditingController();
+    final _passwordController = TextEditingController();
+
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Chat Demo',
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 20),
+              MyTextField(hint: 'Name', controller: _nameController),
+              const SizedBox(height: 20),
+              MyTextField(hint: 'Email', controller: _emailController),
+              const SizedBox(height: 10),
+              MyTextField(hint: 'Password', controller: _passwordController),
+              const SizedBox(height: 10),
+              LoginButton(
+                emailController: _emailController,
+                passwordController: _passwordController,
+              ),
+              const SizedBox(height: 10),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
