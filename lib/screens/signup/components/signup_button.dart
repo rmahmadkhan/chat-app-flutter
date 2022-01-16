106a96746f25b1d5bb80c67657e5bfcc1ef70baa
Ahmad Khan <rmahmadkhan@icloud.com>
2022-01-16 15:51:45 +0500

    Created SignupButton

---
+++ lib/screens/signup/components/signup_button.dart
@@ -0,0 +1,56 @@
+import 'package:chat_app/screens/inbox/inbox_screen.dart';
+import 'package:chat_app/widgets/my_button.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class SignupButton extends StatelessWidget {
+  final TextEditingController nameController;
+  final TextEditingController emailController;
+  final TextEditingController passwordController;
+
+  const SignupButton({
+    required this.nameController,
+    required this.emailController,
+    required this.passwordController,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MyButton(
+      title: 'Login',
+      onPressed: () => onPressed(context),
+    );
+  }
+
+  void onPressed(BuildContext context) async {
+    try {
+      final userCredentials = await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(
+              email: emailController.text, password: passwordController.text);
+
+      final email = userCredentials.user?.email;
+      if (email != null) {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => const InboxScreen()),
+        );
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Could not signup, please try again.')),
+        );
+      }
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('The password provided is too weak.')),
+        );
+      } else if (e.code == 'email-already-in-use') {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+              content: Text('The account already exists for that email.')),
+        );
+      }
+    }
+  }
+}
