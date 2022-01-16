40cc1c67ca2d4f23a2a6a5369e560d49581836ae
Ahmad Khan <rmahmadkhan@icloud.com>
2022-01-16 15:35:44 +0500

    Initialized Firebase in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:chat_app/screens/login/login_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp();
+
   runApp(const MyApp());
 }
 
