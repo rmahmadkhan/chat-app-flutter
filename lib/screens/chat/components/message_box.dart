ceb28e8573a774c8f73dc8334deeba3ad55522e2
Ahmad Khan <rmahmadkhan@icloud.com>
2022-01-25 09:26:56 +0500

    Added vertical padding on MessageBox

--- lib/screens/chat/components/message_box.dart
+++ lib/screens/chat/components/message_box.dart
@@ -14,35 +14,38 @@ class MessageBox extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment:
-          isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
-      children: [
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 5),
-          child: Text(
-            sender,
-            style: const TextStyle(fontSize: 12, color: Colors.black54),
-          ),
-        ),
-        const SizedBox(height: 5),
-        Material(
-          borderRadius: const BorderRadius.all(Radius.circular(30.0)),
-          elevation: 5,
-          color: isMe ? Colors.lightBlueAccent : Colors.white,
-          child: Padding(
-            padding:
-                const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+    return Padding(
+      padding: const EdgeInsets.only(top: 8.0),
+      child: Column(
+        crossAxisAlignment:
+            isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 5),
             child: Text(
-              text,
-              style: TextStyle(
-                color: isMe ? Colors.white : Colors.black,
-                fontSize: 15.0,
+              sender,
+              style: const TextStyle(fontSize: 12, color: Colors.black54),
+            ),
+          ),
+          const SizedBox(height: 5),
+          Material(
+            borderRadius: const BorderRadius.all(Radius.circular(30.0)),
+            elevation: 5,
+            color: isMe ? Colors.lightBlueAccent : Colors.white,
+            child: Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+              child: Text(
+                text,
+                style: TextStyle(
+                  color: isMe ? Colors.white : Colors.black,
+                  fontSize: 15.0,
+                ),
               ),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
